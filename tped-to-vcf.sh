f97d3247f1d6c2e5c6a7919975faaf1393d47882
peter <petrel.harp@gmail.com>
2015-10-10 15:39:55 -0700

    tped to vcf works

--- tped-to-vcf.sh
+++ tped-to-vcf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # from ../../../thedata/POPRES_Genotypes_QC2_v2_TXT.tfam.gz
-TFAM="../everyone.tfam"
+TFAM="$(readlink -f $(dirname $0)/everyone.tfam)"
 
 if ! [ $# -eq 1 ]
 then
@@ -11,27 +11,28 @@ fi
 
 TPED_FILE="$1"
 
-if ! [ -f $TPED_FILE -a -f $TFAM ]
+if ! [ -f $TPED_FILE -a -f ${TFAM:-} ]
 then
-    echo "Can't find" $TPED_FILE
+    echo "Can't find $TPED_FILE or $TFAM"
     exit 1
 fi
 
 TPED="${TPED_FILE%.tped.gz}"
+TPED_NAME=$(basename $TPED)
 # this makes a bunch of things we don't need; put them in here
 TEMPDIR="${TPED}_dir"
 mkdir -p $TEMPDIR
 
 gzip -d $TPED_FILE
 ln -s $TFAM ${TPED}.tfam
-plink --tfile $TPED --noweb --make-bed --out $TEMPDIR/$TPED
+plink --tfile $TPED --noweb --make-bed --out $TEMPDIR/$TPED_NAME
 gzip ${TPED_FILE%.gz}
 
-cd $TEMPDIR || ( echo "can't change directories to $TEMPDIR"; exit 1 )
-pseq $TPED new-project
-pseq ${TPED}.pseq load-plink --file $TPED --id $TPED
-pseq ${TPED}.pseq write-vcf | awk '$5 == "0" { $5="A" }; {print;}' | gzip -c >../${TPED}.vcf.gz
-cd ..
+pushd $TEMPDIR || ( echo "can't change directories to $TEMPDIR"; exit 1 )
+pseq $TPED_NAME new-project
+pseq ${TPED_NAME}.pseq load-plink --file $TPED_NAME --id $TPED_NAME
+pseq ${TPED_NAME}.pseq write-vcf | awk '$5 == "0" { $5="A" }; {print;}' | gzip -c >../${TPED_NAME}.vcf.gz
+popd
 rm -rf $TEMPDIR
 
 echo "All done with $TPED"
