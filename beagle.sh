fafcc9dbd7f7b2a26b440c3d245b4c2ae9455dea
peter <petrel.harp@gmail.com>
2015-10-10 16:03:18 -0700

    .

--- beagle.sh
+++ beagle.sh
@@ -10,21 +10,21 @@ else
     # at home
     JAVA="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
     BEAGLE="/home/peter/software/beagle/beagle.09Oct15.56b.jar"
+
+    if ! [ $# -eq 1 ]
+    then
+        echo "Usage: beagle.sh (.vcf file)"
+        exit 1
+    fi
+
+    VCF_FILE="$1"
 fi
 
 
-if ! [ $# -eq 1 ]
-then
-    echo "Usage: beagle.sh (.vcf file)"
-    exit 1
-fi
-
-VCF_FILE="$1"
-
-if ! [ -f $VCF_FILE ]
+if ! [ -f ${VCF_FILE:-} ]
 then
     echo "Can't find" $VCF_FILE
     exit 1
 fi
 
-$JAVA -jar $BEAGLE gt=$VCF_FILE ibd=true out=${VCF_FILE%%.vcf}.beagle
+$JAVA -jar $BEAGLE gt=$VCF_FILE ibd=true out=${VCF_FILE%%.vcf.gz}.beagle
