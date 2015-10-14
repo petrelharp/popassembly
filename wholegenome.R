afe35ddb030c6669f14503da8e0a89aaf235bbd9
unknown <gexinzhou@pku.edu.cn>
2015-10-14 16:34:27 -0700

    wholegenome.R

--- wholegenome.R
+++ wholegenome.R
@@ -1,4 +1,7 @@
+#This file is to calculate the ibd shared by any two contigs of the whole genome
+#read all the ibd.sorted.file
 setwd("/home/xinzhou/ibdsorted")
+setwd("D:/dy")
 chr<-list()
 id<-integer()
 name<-character()
@@ -12,22 +15,28 @@ for (name in f) {
     }
   }
 }
+
+#calculate the ibd shared by any two contigs
 setwd("/home/xinzhou")
 write.table(contig_table,"contig_table.txt")
-id1<-character()
-id2<-character()
-contig1<-integer()
-contig2<-integer()
-N<-integer()
-A<-data.frame(id1,id2,contig1,contig2,N,stringsAsFactors=FALSE)
-n<-length(chr)
+#read the file that records the country of each individual
+country<-read.csv("sample-info1.csv")
+inds<-read.table("tmp.ids",head=F)
+country.table <- country[,c(2,11)]
+country.table <- subset(country.table,SUBJID%in%inds[,1]&(!is.na(COUNTRY_SELF)))
+A <- data.frame(id1,id2,contig1,contig2,N,stringsAsFactors=FALSE)
+n <- length(chr)
 for (j in 1:(n-1)) {
   for (i in (j+1):n) {
-    B<-intersect(chr[[i]][,9],chr[[j]][,9])
-    B1<-B%%(10^8)
-    B2<-B%/%(10^8)
-    A<-rbind(A,cbind(B1,B2,rep(j,length(B1)),rep(i,length(B2)),rep(1,length(B1))))
-    
+    B <- intersect(chr[[i]][,9],chr[[j]][,9])
+    B1 <- as.integer(B%%(10^8))
+    B2 <- as.integer(B%/%(10^8))
+    #change id to countries and sum up the ibd shared between same pair of countries
+    A<-cbind(B1,B2,rep(j,length(B1)),rep(i,length(B2)),rep(1,length(B1)))
+    country1 <- country.table[B1,country.table[,1]),2]
+    country2 <- country.table[B2,country.table[,1]),2]
+    B.new <- data.frame(country1,country2,rep(j,length(B1)),rep(i,length(B1)),rep(1,length(B1)))
+    N.agg <- aggregate(B.new$N,by=list(B.new$country1,B.new$country2,B.new$contig1,B.new$contig2),FUN="sum")
+    write.table(A,paste("contig_",j,"_contig_",i,"_ibdshared.csv",sep=""),row.names=FALSE,sep=',')    
   }
 }
-write.table(A,"wholegenome.txt")
